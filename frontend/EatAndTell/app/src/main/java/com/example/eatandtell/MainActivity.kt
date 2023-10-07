afb64cdb2eefd8f7568e2a2c903c95bb41b9b0c0
OctoHoon <sakonghoon@naver.com>
2023-10-07 21:17:44 +0900

    feat/Landing Page

--- frontend/EatAndTell/app/src/main/java/com/example/eatandtell/MainActivity.kt
+++ frontend/EatAndTell/app/src/main/java/com/example/eatandtell/MainActivity.kt
@@ -1,28 +1,51 @@
+// MainActivity.kt
 package com.example.eatandtell
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.eatandtell.ui.signup.SignupActivity
 import com.example.eatandtell.ui.theme.EatAndTellTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             EatAndTellTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Logo()
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        LaunchedEffect(key1 = Unit) {
+                            // 기다릴 시간(1초)
+                            delay(1000)
+
+                            // 1초 후에 SignupActivity로 이동
+                            startActivity(Intent(this@MainActivity, SignupActivity::class.java))
+                            finish()
+                        }
+                    }
                 }
             }
         }
@@ -30,17 +53,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Logo() {
+    Image(
+        painter = painterResource(R.drawable.logo),
+        contentDescription = "Logo Image",
+        modifier = Modifier
+            .width(210.dp)
+            .height(30.dp)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EatAndTellTheme {
-        Greeting("Android")
-    }
+    )
 }
---
+++ frontend/EatAndTell/app/src/main/java/com/example/eatandtell/ui/signup/SignupActivity.kt
@@ -0,0 +1,48 @@
+// SignupActivity.kt
+package com.example.eatandtell.ui.signup
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
+
+
+class SignupActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+
+                Text("Sign Up")
+
+            }
+            }
+        }
+    }
