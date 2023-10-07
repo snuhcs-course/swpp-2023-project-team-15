e52d3809298d43f6d4addb7e099556cda866e8b0
Jaechan <dlwocks31@gmail.com>
2023-10-07 14:48:50 +0900

    feat: Add AWS RDS integration

--- backend/README.md
+++ backend/README.md
@@ -8,6 +8,16 @@ Then navigate to backend root and run:
 
 `poetry install`
 
+# Environment Variable
+Get your environment variable from https://www.notion.so/Environment-Variable-22dcf6d95d294957a6e9b518005cc4ca?pvs=4
+
+```
+DB_HOST=xxxx
+DB_USER=xxxx
+DB_PASSWORD=xxxx
+```
+
+
 
 # Migrate
 `poetry run python manage.py migrate`
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
 from pathlib import Path
+
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +78,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'swpp_team_15',  # name of the database on RDS
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '3306',
     }
 }
 
