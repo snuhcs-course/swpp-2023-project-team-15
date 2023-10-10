211425ec61425dfe8fb6fcb9644881fe04371104
jihun189 <alexjihun@snu.ac.kr>
2023-10-10 19:57:15 +0900

    feat: create posts POST GET DELETE API

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
+    path("posts/", include("posts.urls"))
 ]
