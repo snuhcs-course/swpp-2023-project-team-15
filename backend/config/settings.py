0021a25e620d8313dc5ad4ddc80a0995058ec46c
Jaechan <dlwocks31@gmail.com>
2023-10-10 17:46:21 +0900

    fix: add ALLOWED_HOST settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-jq=5xo87qb&%g^kwjcn5162tty82@wk8(!v$e%-m_h)lv1=eh_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ec2-13-125-91-166.ap-northeast-2.compute.amazonaws.com"]
+ALLOWED_HOSTS = ["ec2-13-125-91-166.ap-northeast-2.compute.amazonaws.com", "127.0.0.1"]
 
 
 # Application definition
