9ca588febdf7b3f5a8738bfd0e20e4e5899fb520
Jaechan <dlwocks31@gmail.com>
2023-10-11 11:03:51 +0900

    feat: add models to admin

--- backend/posts/admin.py
+++ backend/posts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, PostPhoto, Restaurant
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'restaurant_id', 'title', 'rating', 'created_at']
+    search_fields = ['title', 'description']
+    list_filter = ['created_at', 'rating']
+    ordering = ['-created_at']
+
+@admin.register(PostPhoto)
+class PostPhotoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'post', 'photo_url']
+    search_fields = ['photo_url']
+    list_filter = ['post']
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    search_fields = ['name']
+    list_filter = ['name']
