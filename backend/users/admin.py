966f7f0527e4e9a23dc3a6ec7663af90813e8341
OctoHoon <sakonghoon@naver.com>
2023-10-19 18:15:41 +0900

    feat: register  user admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import User
+
 
 # Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (
+            "Profile",
+            {
+                "fields": (
+                    "avatar_url",
+                    "username",
+                    "password",
+                    "email",
+                    "description",
+                ),
+            },
+        ),
+        # (
+        #     "Permissions",
+        #     {
+        #         "fields": (
+        #             "is_superuser",
+        #             "groups",
+        #             "user_permissions",
+        #         ),
+        #         "classes": ("collapse",),
+        #     },
+        # ),
+        # (
+        #     "Important Dates",
+        #     {
+        #         "fields": ("last_login", "date_joined"),
+        #         "classes": ("collapse",),
+        #     },
+        # ),
+    )
+
+    list_display = (
+        "username",
+        "description",
+        "avatar_url",
+    )
