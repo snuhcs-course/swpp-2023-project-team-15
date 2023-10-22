c15bfb42790b2f0fcdfc8be6678346690c4b7e88
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2023-10-22 11:08:21 +0900

    chore: update so that @ for user and empty string for restaurant

--- frontend/EatAndTell/app/src/main/java/com/example/eatandtell/ui/appmain/SearchScreen.kt
+++ frontend/EatAndTell/app/src/main/java/com/example/eatandtell/ui/appmain/SearchScreen.kt
@@ -95,22 +95,26 @@ fun SearchScreen(navController: NavHostController, context: ComponentActivity, v
             if (triggerSearch) {
                 loading = true
                 try {
-                    viewModel.getFilteredUsers(
-                        searchText.text,
-                        onSuccess = { users ->
-                            userLists = users // resulted user Lists
-                            loading = false
-                        },
-                    )
-                    viewModel.getFilteredByRestaurants(
-                        searchText.text,
-                        onSuccess = { posts ->
-                            postLists = posts // resulted post Lists
-                            loading = false
-                        },
-                    )
-                }
-                catch (e: Exception) {
+                    if(searchText.text.startsWith("@")) { // If search starts with @
+                        viewModel.getFilteredUsers(
+                            searchText.text.drop(1), // Remove @ from the search string
+                            onSuccess = { users ->
+                                userLists = users // resulted user Lists
+                                loading = false
+                            }
+                        )
+                        postLists = emptyList() // Reset post lists
+                    } else {
+                        viewModel.getFilteredByRestaurants(
+                            searchText.text,
+                            onSuccess = { posts ->
+                                postLists = posts // resulted post Lists
+                                loading = false
+                            }
+                        )
+                        userLists = emptyList() // Reset user lists
+                    }
+                } catch (e: Exception) {
                     println("searchload error")
                     showToast(context, "search 로딩에 실패하였습니다")
                     loading = false
@@ -132,33 +136,34 @@ fun SearchScreen(navController: NavHostController, context: ComponentActivity, v
                 )
             }
         }
-        else{
-            //for users
+        else {
             LazyColumn(
                 state = rememberLazyListState(),
-                modifier = Modifier
-                    .fillMaxSize()
+                modifier = Modifier.fillMaxSize()
             ) {
-                items(userLists.size) { index ->
-                    val user = userLists[index]
-                    Profile(
-                        profileUrl = user.avatar_url,
-                        username = user.username,
-                        userDescription = user.description,
-                        onImageClick = {
-                            navController.navigate("Profile/${user.id}")
-                        },
-                        onDescriptionClick = {
-                            navController.navigate("Profile/${user.id}")
-                        },
-                        onUsernameClick = {
-                            navController.navigate("Profile/${user.id}")
-                        }
-                    )
-                }
-                items(postLists.size) { index ->
-                    val post = postLists[index]
-                    HomePost(post, viewModel = viewModel, navHostController = navController, myInfo = post.user)
+                if (searchText.text.startsWith("@")) {
+                    items(userLists.size) { index ->
+                        val user = userLists[index]
+                        Profile(
+                            profileUrl = user.avatar_url,
+                            username = user.username,
+                            userDescription = user.description,
+                            onImageClick = {
+                                navController.navigate("Profile/${user.id}")
+                            },
+                            onDescriptionClick = {
+                                navController.navigate("Profile/${user.id}")
+                            },
+                            onUsernameClick = {
+                                navController.navigate("Profile/${user.id}")
+                            }
+                        )
+                    }
+                } else {
+                    items(postLists.size) { index ->
+                        val post = postLists[index]
+                        HomePost(post, viewModel = viewModel, navHostController = navController, myInfo = post.user)
+                    }
                 }
             }
         }
