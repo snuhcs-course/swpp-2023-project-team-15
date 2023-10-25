b6e1591b523f2d2a74f55b25c01bd69f4bbd7fad
OctoHoon <sakonghoon@naver.com>
2023-10-25 22:12:37 +0900

    feat: add type choice field to Tag model

--- backend/tags/admin.py
+++ backend/tags/admin.py
@@ -6,6 +6,6 @@ from tags.models import Tag
 # Register your models here.
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = ['id', 'ko_label', 'en_label']
+    list_display = ['id', 'type', 'ko_label', 'en_label']
     search_fields = ['ko_label', 'en_label']
     list_filter = ['ko_label', 'en_label']
